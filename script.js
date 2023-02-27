f499a5e0e6a97b9c7ba6c3259040b498a7c9c0f4
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-02-26 23:45:35 -0300

    Responsivo para mobile

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const nav = document.querySelector('nav');
 window.addEventListener("scroll", function(){
     nav.classList.toggle("sticky", window.scrollY > 50)
 });
+
+let menu = document.querySelector('#menu-icon'),
+    navitems = document.querySelector('.navItems'),
+    navicons = document.querySelector('.icons');
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navitems.classList.toggle('open');
+    navicons.classList.toggle('open');
+};
+
+window.onscroll= () =>{
+    menu.classList.remove('bx-x');
+    navitems.classList.remove('open');
+    navicons.classList.remove('open');
+};
