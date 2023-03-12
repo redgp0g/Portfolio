f5631f85eb817bebde02ca31358b8dc72c2bfde5
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-03-12 14:19:02 -0300

    Mudança na escrita do typingeffect

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ window.addEventListener("scroll", function(){
     nav.classList.toggle("sticky", window.scrollY > 50)
 });
 
+var typingEffect = new Typed(".multiText",{
+    strings : ["programador","designer","autodidata","leitor","atleta"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1500
+});
+
 let menu = document.querySelector('#menu-icon'),
     navitems = document.querySelector('.navItems'),
     navicons = document.querySelector('.icons');
@@ -17,3 +25,6 @@ window.onscroll= () =>{
     navitems.classList.remove('open');
     navicons.classList.remove('open');
 };
+document.getElementById('baixarcurriculo').addEventListener('click', () => {
+    alert('Currículo não adicionado');
+});
